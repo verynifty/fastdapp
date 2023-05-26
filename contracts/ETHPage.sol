297dc9ce6172fcbccd28d5efd68728eeef2337eb
grandsmarquis <jdourlens@gmail.com>
2023-05-26 20:44:22 +0900

    test encoding

--- contracts/ETHPage.sol
+++ contracts/ETHPage.sol
@@ -16,6 +16,12 @@ contract ETHPage is ERC721, ERC721Enumerable, AccessControl {
     mapping(uint256 => string) private mStyle;
     mapping(uint256 => string) private mScript;
 
+    /**
+     * @dev Base64 Encoding/Decoding Table
+     */
+    string internal constant _TABLE =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
     constructor() ERC721("B", "B") {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(MINTER_ROLE, msg.sender);
@@ -49,16 +55,34 @@ contract ETHPage is ERC721, ERC721Enumerable, AccessControl {
         return super.supportsInterface(interfaceId);
     }
 
-    function concatHTML(string memory _body, string memory _style, string memory _script) public pure returns (string memory) {
-        return string(
-            abi.encodePacked('<!DOCTYPE html><html><head><script src="https://cdn.tailwindcss.com"></script></head><body>', _body,'</body><style>', _style,'</style><script>', _script,'</script></html>'));
+    function concatHTML(
+        string memory _body,
+        string memory _style,
+        string memory _script
+    ) public pure returns (string memory) {
+        return
+            string(
+                abi.encodePacked(
+                    '<!DOCTYPE html><html><head><script src="https://cdn.tailwindcss.com"></script></head><body>',
+                    _body,
+                    "</body><style>",
+                    _style,
+                    "</style><script>",
+                    _script,
+                    "</script></html>"
+                )
+            );
     }
 
     function getHTML(uint256 _tokenId) public view returns (string memory) {
         return concatHTML(mBody[_tokenId], mStyle[_tokenId], mScript[_tokenId]);
     }
 
-    function craft(string memory _body, string memory _style, string memory _script) public {
+    function craft(
+        string memory _body,
+        string memory _style,
+        string memory _script
+    ) public {
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(msg.sender, tokenId);
@@ -70,19 +94,95 @@ contract ETHPage is ERC721, ERC721Enumerable, AccessControl {
     function tokenURI(
         uint256 _tokenId
     ) public view virtual override returns (string memory) {
-        string memory description = 'B';
-        string memory name = 'B';
+        string memory description = "B";
+        string memory name = "B";
         string memory json = string(
             abi.encodePacked(
                 "{name:'",
                 name,
                 "', description:'",
                 description,
-                "', animation_url:'",
-                getHTML(_tokenId),
+                "', animation_url:'data:text/html;base64,",
+                encodeBase64(bytes(getHTML(_tokenId))),
                 "'}"
             )
         );
         return json;
+    }
+
+      /**
+     * @dev Converts a `bytes` to its Bytes64 `string` representation.
+     */
+    function encodeBase64(bytes memory data) internal pure returns (string memory) {
+        /**
+         * Inspired by Brecht Devos (Brechtpd) implementation - MIT licence
+         * https://github.com/Brechtpd/base64/blob/e78d9fd951e7b0977ddca77d92dc85183770daf4/base64.sol
+         */
+        if (data.length == 0) return "";
+
+        // Loads the table into memory
+        string memory table = _TABLE;
+
+        // Encoding takes 3 bytes chunks of binary data from `bytes` data parameter
+        // and split into 4 numbers of 6 bits.
+        // The final Base64 length should be `bytes` data length multiplied by 4/3 rounded up
+        // - `data.length + 2`  -> Round up
+        // - `/ 3`              -> Number of 3-bytes chunks
+        // - `4 *`              -> 4 characters for each chunk
+        string memory result = new string(4 * ((data.length + 2) / 3));
+
+        /// @solidity memory-safe-assembly
+        assembly {
+            // Prepare the lookup table (skip the first "length" byte)
+            let tablePtr := add(table, 1)
+
+            // Prepare result pointer, jump over length
+            let resultPtr := add(result, 32)
+
+            // Run over the input, 3 bytes at a time
+            for {
+                let dataPtr := data
+                let endPtr := add(data, mload(data))
+            } lt(dataPtr, endPtr) {
+
+            } {
+                // Advance 3 bytes
+                dataPtr := add(dataPtr, 3)
+                let input := mload(dataPtr)
+
+                // To write each character, shift the 3 bytes (18 bits) chunk
+                // 4 times in blocks of 6 bits for each character (18, 12, 6, 0)
+                // and apply logical AND with 0x3F which is the number of
+                // the previous character in the ASCII table prior to the Base64 Table
+                // The result is then added to the table to get the character to write,
+                // and finally write it in the result pointer but with a left shift
+                // of 256 (1 byte) - 8 (1 ASCII char) = 248 bits
+
+                mstore8(resultPtr, mload(add(tablePtr, and(shr(18, input), 0x3F))))
+                resultPtr := add(resultPtr, 1) // Advance
+
+                mstore8(resultPtr, mload(add(tablePtr, and(shr(12, input), 0x3F))))
+                resultPtr := add(resultPtr, 1) // Advance
+
+                mstore8(resultPtr, mload(add(tablePtr, and(shr(6, input), 0x3F))))
+                resultPtr := add(resultPtr, 1) // Advance
+
+                mstore8(resultPtr, mload(add(tablePtr, and(input, 0x3F))))
+                resultPtr := add(resultPtr, 1) // Advance
+            }
+
+            // When data `bytes` is not exactly 3 bytes long
+            // it is padded with `=` characters at the end
+            switch mod(mload(data), 3)
+            case 1 {
+                mstore8(sub(resultPtr, 1), 0x3d)
+                mstore8(sub(resultPtr, 2), 0x3d)
+            }
+            case 2 {
+                mstore8(sub(resultPtr, 1), 0x3d)
+            }
+        }
+
+        return result;
     }
 }
