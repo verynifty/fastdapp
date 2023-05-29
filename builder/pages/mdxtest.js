08fe564c87708e9820274ce6fbe212eabf9e30b2
grandsmarquis <jdourlens@gmail.com>
2023-05-30 00:01:58 +0900

    mdxtest scope

--- builder/pages/mdxtest.js
+++ builder/pages/mdxtest.js
@@ -1,6 +1,10 @@
-import React, { useRef } from 'react';
+'use client'
+import React, { useRef, Suspense } from 'react';
 import { serialize } from 'next-mdx-remote/serialize'
-import { MDXRemote } from 'next-mdx-remote/rsc'
+import { MDXRemote } from 'next-mdx-remote'
+
+import { watchAccount, getAccount } from '@wagmi/core'
+
 
 
 import Editor from '@monaco-editor/react';
@@ -28,14 +32,20 @@ export default function TestPage({ source }) {
     const [content, setContent] = React.useState(0);
     let hello = "hello";
 
-            /*<Editor height="90vh" defaultLanguage="javascript" onChange={handleEditorChange}
-                onMount={handleEditorDidMount} defaultValue="// some comment" />;*/
+    let account = getAccount();
+
+    let scope = { account: account, hello: hello };
+
+    /*<Editor height="90vh" defaultLanguage="javascript" onChange={handleEditorChange}
+        onMount={handleEditorDidMount} defaultValue="// some comment" />;*/
 
 
     return (
         <div className="wrapper">
             <div className="wrapper">
-                <MDXRemote source={hello} components={components} />
+            <Suspense>
+                <MDXRemote {...source} components={components} scope={scope}/>
+                </Suspense>
             </div>
 
 
@@ -46,12 +56,16 @@ export default function TestPage({ source }) {
 export async function getStaticProps() {
     // MDX text - can be from a local file, database, anywhere
     const source = `
+    {a = 2}
     Some **mdx** text, with a component 
     # BlockNumber
     <BN /> 
     # Balance
-    <Balance address={'0x4B5922ABf25858d012d12bb1184e5d3d0B6D6BE4'} />
-    `   
+    <Balance address={'0x4B5922ABf25858d012d12bb1184e5d3d0B6D6BE4'} /> haha
+    {a}
+    # h
+    {account.address}
+    `
     const mdxSource = await serialize(source)
     return { props: { source: mdxSource } }
 }
