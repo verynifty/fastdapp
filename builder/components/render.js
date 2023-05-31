d4993975c79250c9c7df2c2dc52f50c36ca32da1
grandsmarquis <jdourlens@gmail.com>
2023-05-31 20:27:35 +0900

    send tx from componentn

--- builder/components/render.js
+++ builder/components/render.js
@@ -8,7 +8,7 @@ import { default as React, useState, useRef } from 'react';
 
 import BN from 'components/bn';
 import Balance from 'components/balance';
-import SendTransaction from 'components/sendTransaction';
+import SendTransaction from 'components/sendTransactionTwo';
 
 let value = "// some comment";
 const components = { "BN": BN, "Balance": Balance, "SendTransaction": SendTransaction }
--- builder/pages/_app.js
+++ builder/pages/_app.js
@@ -7,46 +7,50 @@ import '@rainbow-me/rainbowkit/styles.css';
 
 import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
 import { AppProps } from 'next/app';
-import { configureChains, createConfig, WagmiConfig } from 'wagmi';
+import { configureChains, createConfig, WagmiConfig, useProvider } from 'wagmi';
 import { arbitrum, goerli, mainnet, optimism, polygon } from 'wagmi/chains';
 import { publicProvider } from 'wagmi/providers/public';
 
+import { InjectedConnector } from 'wagmi/connectors/injected'
+import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
+import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
+
+
 const { chains, publicClient, webSocketPublicClient } = configureChains(
-  [
-    mainnet,
-    polygon,
-    optimism,
-    arbitrum,
-    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [goerli] : []),
-  ],
-  [publicProvider()]
+    [
+        mainnet,
+        polygon,
+        optimism,
+        arbitrum,
+        goerli
+    ],
+    [publicProvider()]
 );
 
-const { connectors } = getDefaultWallets({
-  appName: 'RainbowKit App',
-  projectId: 'YOUR_PROJECT_ID',
-  chains,
-});
+const connectors = [
+    new InjectedConnector({ chains }),
+    new MetaMaskConnector({ chains }),
+];
+
 
 const wagmiConfig = createConfig({
-  autoConnect: true,
-  connectors,
-  publicClient,
-  webSocketPublicClient
+    autoConnect: true,
+    connectors,
+    publicClient,
+    webSocketPublicClient
 });
 
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 
-
 function MyApp({ Component, pageProps }) {
-  return (
-    <WagmiConfig config={wagmiConfig}>
-      <RainbowKitProvider chains={chains}>
-      <ConnectButton />
-        <Component {...pageProps} />
-      </RainbowKitProvider>
-    </WagmiConfig>
-  );
+    return (
+        <WagmiConfig config={wagmiConfig}>
+            <RainbowKitProvider chains={chains} showRecentTransactions={true}>
+                    <ConnectButton />
+                    <Component {...pageProps} />
+            </RainbowKitProvider>
+        </WagmiConfig>
+    );
 }
 
 export default MyApp;
