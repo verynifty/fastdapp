9e685a438e8b40d20f0e76ff44850512fd7baa2a
grandsmarquis <jdourlens@gmail.com>
2023-06-01 14:41:50 +0900

    clean tx button

--- builder/components/sendTransaction.js
+++ builder/components/sendTransaction.js
@@ -1,6 +1,8 @@
 import { default as React, useState, useRef, useEffect } from 'react';
-import { sendTransaction, waitForTransaction } from '@wagmi/core'
+import { usePrepareSendTransaction, sendTransaction, waitForTransaction } from 'wagmi'
 import { useAddRecentTransaction } from '@rainbow-me/rainbowkit';
+import SendTransactionButton from 'components/internals/sendTransactionButton';
+import Test from 'components/internals/test';
 
 const SendTransaction = (props) => {
     const addRecentTransaction = useAddRecentTransaction();
@@ -8,56 +10,18 @@ const SendTransaction = (props) => {
 
     const [to, setTo] = React.useState(props.to);
     const [amount, setAmount] = React.useState(0);
-    const [isLoading, setIsLoading] = React.useState(false);
-    const [pendingTransaction, setPendingTransaction] = React.useState(null);
+
     // This will run only once
     useEffect(() => {
 
     }, []);
 
-    const onClickSend = async () => {
-        setIsLoading(true);
-        try {
-            const tx = await sendTransaction({
-                to: to,
-                value: amount,
-            })
-            setPendingTransaction(tx.hash);
-            addRecentTransaction({
-                hash: tx.hash,
-                description: 'Sending a transaction to ' + to + ' for ' + amount + ' ETH',
-            });
-            const data = await waitForTransaction({
-                hash: tx.hash,
-            })
-            setIsLoading(false);
-        } catch (error) {
-            console.log(error)
-            setIsLoading(false);
-        }
-    }
-
-    const button = () => {
-        if (!isLoading) {
-            return (
-                <button onClick={onClickSend} class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                    Send
-                </button>
-            )
-        } else {
-            return (
-                <div role="status">
-                    <svg aria-hidden="true" class="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
-                        <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
-                    </svg>
-                    <span>{pendingTransaction}</span>
-                    <span class="sr-only">Loading...</span>
-                </div>
-            )
-        }
-    }
-
+    const { config } = usePrepareSendTransaction({
+      request: {
+        to: to,
+        value: amount,
+      },
+    })
 
     return (
         <React.Fragment>
@@ -76,8 +40,7 @@ const SendTransaction = (props) => {
                     </div>
                 </div>
                 <div class="sm:col-span-12">
-                    {button()}
-
+                    <SendTransactionButton transactionDescription={'Send ' + amount + ' to ' + to} transaction={usePrepareSendTransaction({to:to, value: amount}).config} />
                 </div>
             </div >
         </React.Fragment>
