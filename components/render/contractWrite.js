518c68e433f91b97737d3f06dda5f205aac8f6ed
grandsmarquis <jdourlens@gmail.com>
2023-06-10 14:19:37 +0900

    contract write has dynamic parameters

--- components/render/contractWrite.js
+++ components/render/contractWrite.js
@@ -8,6 +8,18 @@ const WriteContract = (props) => {
     const [formatted, setFormatted] = React.useState("");
     const [symbol, setSymbol] = React.useState("");
     const args = props.args || [];
+    const argsStateValues = [];
+    const argsStateSetters = [];
+
+    function getFunction() {
+        return props.abi.find((element) => element.name === props.functionName && element.type === "function");
+    }
+
+    for (const [index, input] of getFunction().inputs.entries()) {
+        const [value, setter] = React.useState(args[index]);
+        argsStateValues.push(value);
+        argsStateSetters.push(setter);
+    }
 
     console.log("ABI", props.abi)
 
@@ -17,14 +29,12 @@ const WriteContract = (props) => {
 
     }, []);
 
-    function getFunction() {
-        return props.abi.find((element) => element.name === props.functionName);
-    }
+    
 
     function makeForm() {
         return (
             <div>
-                {getFunction().inputs.map((input) => {
+                {getFunction().inputs.map((input, index) => {
                     return (
                         <div>
                             <label htmlFor={input.name} className="block text-sm font-medium leading-6 text-gray-900">
@@ -32,11 +42,13 @@ const WriteContract = (props) => {
                             </label>
                             <div className="mt-2">
                                 <input
-                                    type="email"
+                                    type="text"
                                     name={input.name}
                                     id={input.name}
                                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                     placeholder="you@example.com"
+                                    value={argsStateValues[index]} 
+                                    onChange={e => argsStateSetters[index](e.target.value)}
                                 />
                             </div>
                         </div>
@@ -49,7 +61,7 @@ const WriteContract = (props) => {
     return (
         <span>
             {makeForm()}
-            <SendTransactionButton text={props.text != null ? props.text : props.functionName} transactionDescription={props.functionName} transaction={usePrepareContractWrite({ address: props.address, abi: props.abi, functionName: props.functionName, args: args }).config} />
+            <SendTransactionButton text={props.text != null ? props.text : props.functionName} transactionDescription={props.functionName} transaction={usePrepareContractWrite({ address: props.address, abi: props.abi, functionName: props.functionName, args: argsStateValues }).config} />
         </span >
     );
 }
