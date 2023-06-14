e5bbbea55b6f90c5b864dccf7e6220d76b413dc3
grandsmarquis <jdourlens@gmail.com>
2023-06-14 15:40:47 +0900

    documentation links

--- webapp/components/Footer.jsx
+++ webapp/components/Footer.jsx
@@ -12,18 +12,16 @@ export function Footer() {
           <Logo className="mx-auto h-10 w-auto" />
           <nav className="mt-10 text-sm" aria-label="quick links">
             <div className="-my-1 flex justify-center gap-x-6">
-              <NavLink href="#features">Features</NavLink>
-              <NavLink href="#testimonials">Testimonials</NavLink>
-              <NavLink href="#pricing">Pricing</NavLink>
+              <NavLink href="https://builddocs.musedao.io/docs/category/components">Documentation</NavLink>
             </div>
           </nav>
         </div>
         <div className="flex flex-col items-center border-t border-slate-400/10 py-10 sm:flex-row-reverse sm:justify-between">
           <div className="flex gap-x-6">
             <Link
-              href="https://twitter.com"
+              href="https://twitter.com/NiftyMuseum"
               className="group"
-              aria-label="TaxPal on Twitter"
+              aria-label="Muse DAO on Twitter"
             >
               <svg
                 aria-hidden="true"
@@ -33,9 +31,9 @@ export function Footer() {
               </svg>
             </Link>
             <Link
-              href="https://github.com"
+              href="https://github.com/verynifty/etherpage"
               className="group"
-              aria-label="TaxPal on GitHub"
+              aria-label="on GitHub"
             >
               <svg
                 aria-hidden="true"
@@ -46,8 +44,7 @@ export function Footer() {
             </Link>
           </div>
           <p className="mt-6 text-sm text-slate-500 sm:mt-0">
-            Copyright &copy; {new Date().getFullYear()} TaxPal. All rights
-            reserved.
+           A Muse DAO venture
           </p>
         </div>
       </Container>
--- webapp/pages/index.js
+++ webapp/pages/index.js
@@ -36,6 +36,8 @@ export default function Home() {
         <CallToAction />
 
       </main>
+      <Footer />
+
     </>
   )
 }
