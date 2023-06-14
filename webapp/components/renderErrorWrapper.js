559b82603e98499757cf5c537d82565da0e4a430
grandsmarquis <jdourlens@gmail.com>
2023-06-14 15:56:55 +0900

    fix error state

--- webapp/components/renderErrorWrapper.js
+++ webapp/components/renderErrorWrapper.js
@@ -10,11 +10,12 @@ class RenderErrorWrapper extends React.Component {
     static getDerivedStateFromError(error) {
       // Update state so the next render will show the fallback UI
         
-      return { hasError: true, error: error }
+      return { hasError: true }
     }
     componentDidCatch(error, errorInfo) {
       // You can use your own error logging service here
       console.log({ error, errorInfo })
+      this.state.error = error;
     }
 
     componentDidUpdate(prevProps) {
