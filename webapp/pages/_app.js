3d6e7ea14e54bc2848c858336745473ae790d6da
grandsmarquis <jdourlens@gmail.com>
2023-06-15 18:13:07 +0900

    test

--- webapp/pages/_app.js
+++ webapp/pages/_app.js
@@ -86,6 +86,15 @@ function MyApp({ Component, pageProps }) {
         >
             <WagmiConfig config={wagmiConfig}>
                 <RainbowKitProvider chains={chains} showRecentTransactions={true}>
+                    <!-- Google tag (gtag.js) -->
+                    <script async src="https://www.googletagmanager.com/gtag/js?id=G-SV30RLSQGW"></script>
+                    <script>
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+
+                        gtag('config', 'G-SV30RLSQGW');
+                    </script>
                     <GoogleAnalytics trackPageViews />
                     <Header />
                     <Component {...pageProps} />
