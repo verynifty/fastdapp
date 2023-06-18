5b57c762a149a90fedcf234859e4127214235bba
grandsmarquis <jdourlens@gmail.com>
2023-06-18 14:51:39 +0900

    refactor

--- webapp/tailwind.config.js
+++ webapp/tailwind.config.js
@@ -16,42 +16,8 @@ module.exports = {
   },
   plugins: [
     require('@tailwindcss/forms'),
-    require("@tailwindcss/typography"),
-    require("daisyui")
+    require("@tailwindcss/typography")
   ],
   safelist: [
-  ],
-  daisyui: {
-    themes: [
-      "light",
-      "dark",
-      "cupcake",
-      "bumblebee",
-      "emerald",
-      "corporate",
-      "synthwave",
-      "retro",
-      "cyberpunk",
-      "valentine",
-      "halloween",
-      "garden",
-      "forest",
-      "aqua",
-      "lofi",
-      "pastel",
-      "fantasy",
-      "wireframe",
-      "black",
-      "luxury",
-      "dracula",
-      "cmyk",
-      "autumn",
-      "business",
-      "acid",
-      "lemonade",
-      "night",
-      "coffee",
-      "winter"
-    ]
-  }
+  ]
 }
