f77a398802eb748e0eb7f02e31e01b34222bb223
grandsmarquis <jdourlens@gmail.com>
2023-06-21 13:21:50 +0900

    better publishing flow

--- webapp/components/publish.js
+++ webapp/components/publish.js
@@ -8,6 +8,9 @@ const Publish = (props) => {
     const [isLoading, setIsLoading] = React.useState(false);
     const client = new Web3Storage({ token: process.env.NEXT_PUBLIC_WEB3STORAGE_TOKEN })
     const router = useRouter()
+    const [IPFS, setIPFS] = React.useState("");
+    const [pageLink, setPageLink] = React.useState("");
+    const [isUploaded, setIsUploaded] = React.useState(false);
 
     // This will run only once
     useEffect(() => {
@@ -32,8 +35,10 @@ const Publish = (props) => {
                 await new Promise(r => setTimeout(r, 1000));
                 let file_url = "https://" + cid + ".ipfs.w3s.link/" + filename
                 let f = await axios.get(file_url)
-                router.replace("/page?ipfs=" + encodeURIComponent(path), "/page?ipfs=" + encodeURIComponent(path), { query: { ipfs: path } })
+                setIPFS(path)
+                setPageLink("https://build.musedao.io/page?ipfs=" + encodeURIComponent(path))
                 setIsLoading(false);
+                setIsUploaded(true);
                 return;
             } catch (error) {
                 console.log("File not uploaded yet...")
@@ -41,23 +46,51 @@ const Publish = (props) => {
         }
     }
 
-    function button() {
-        if (isLoading) {
+    function content() {
+        if (isUploaded) {
             return (
-                <button
-                    className="btn" disabled="disabled"
-                >
-                    uploading...
-                </button>
+                <div>
+                    <center><h3 class="text-lg font-bold">Your app is published 🥳</h3></center>
+                    <div className="form-control w-full">
+                        <label className="label">
+                            <span className="label-text">IPFS Link:</span>
+                        </label>
+                        <input type="text" placeholder="Type here" className="input input-bordered w-full" value={IPFS} />
+                    </div>
+                    <div className="form-control w-full">
+                        <label className="label">
+                            <span className="label-text">Page Link:</span>
+                        </label>
+                        <input type="text" placeholder="Type here" className="input input-bordered w-full" value={pageLink} />
+                    </div>
+                    <a href={pageLink} className="btn mt-4 w-full">Go to your app</a>
+                </div>
+            )
+        }
+        else if (isLoading) {
+            return (
+                <div>
+                     <center><h3 class="text-lg font-bold">Publish your app</h3></center>
+
+                    <button
+                        className="btn" disabled="disabled"
+                    >
+                        Publishing...
+                    </button>
+                </div>
             )
         } else {
             return (
-                <button
-                    onClick={upload}
-                    className="btn"
-                >
-                    Upload
-                </button>
+                <div>
+                    <center><h3 class="text-lg font-bold">Publish your app</h3></center>
+
+                    <button
+                        onClick={upload}
+                        className="btn"
+                    >
+                        Publish
+                    </button>
+                </div>
             )
         }
     }
@@ -68,8 +101,7 @@ const Publish = (props) => {
             <input type="checkbox" id="my_modal_7" class="modal-toggle" />
             <div class="modal">
                 <div class="modal-box">
-                    <h3 class="text-lg font-bold">Publish your app</h3>
-                    {button()}
+                    {content()}
                 </div>
                 <label class="modal-backdrop" for="my_modal_7">Close</label>
             </div>
