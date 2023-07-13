efd1e691eccf248397ab4c36c077971788b3479e
grandsmarquis <jdourlens@gmail.com>
2023-07-13 12:59:27 +0900

    fix header

--- webapp/components/Header.jsx
+++ webapp/components/Header.jsx
@@ -13,7 +13,7 @@ import { ConnectButton } from '@rainbow-me/rainbowkit';
 
 export function Header() {
   return (
-    <div className="navbar bg-base-100">
+    <div className="navbar bg-base-100" style={{"height":"4rem !important"}}>
       <div className="navbar-start">
         <div className="dropdown">
           <label tabIndex={0} className="btn btn-ghost lg:hidden">
