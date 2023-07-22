f389801f3b1dd9f6e4f5efd6487f968c3004d499
grandsmarquis <jdourlens@gmail.com>
2023-07-22 12:56:00 +0900

    cors

--- webapp/pages/app/[name]/[path].js
+++ webapp/pages/app/[name]/[path].js
@@ -7,6 +7,12 @@ export default function MarketData(props) {
 export const getServerSideProps = async ({ req, res, resolvedUrl }) => {
     console.log(req.query)
     res.setHeader("Content-Type", "application/json");
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Content-Type, Authorization, access_token'
+    )
     res.write(JSON.stringify({
         "name": "Fast Dapp",
         "description": "Developing Dapps frontend in minutes",
