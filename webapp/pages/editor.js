c375afe3be21e7791202cdfc3f9ba860597652a0
grandsmarquis <jdourlens@gmail.com>
2023-07-28 11:03:16 +0900

    gradient

--- webapp/pages/editor.js
+++ webapp/pages/editor.js
@@ -205,7 +205,7 @@ export default function EditorPage({ source }) {
                 onKeyUp={onKeyUp.bind(this)}
             >
                 <Publish content={rendered} />
-                <div className="bg-gradient-to-r from-sky-400 to-blue-500 p-2 md:flex md:items-center md:justify-between" style={{ "height": "2rem" }}>
+                <div className="bg-gradient-to-r from-primary to-secondary p-2 md:flex md:items-center md:justify-between" style={{ "height": "2rem" }}>
                     <div className="min-w-0 flex-1">
                         <h2 className="text-xl font-bold  text-white sm:truncate  sm:tracking-tight">
                             Editor
