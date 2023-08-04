0ee4bf923814dd8f07df0983c77a67bce35069c4
grandsmarquis <jdourlens@gmail.com>
2023-08-03 22:42:01 -0400

    contract

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -2,20 +2,10 @@
  * @type import('hardhat/config').HardhatUserConfig
  */
 
-require("@nomiclabs/hardhat-ethers");
+require("@nomicfoundation/hardhat-toolbox");
 
-require("@nomiclabs/hardhat-etherscan");
+require("@nomicfoundation/hardhat-verify");
 require("dotenv").config();
-
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
 
 //
 // Select the network you want to deploy to here:
---
+++ examples/fastname.md
@@ -0,0 +1,15 @@
+---
+chain: 1
+authors: grands_marquis
+theme: fastdapp
+---
+
+# Get your FastDapp name for 0.02 ETH
+
+Use your FastDapp NFT to reserve a domain name here like fasdapp.xyz/name.
+
+With the NFT you can control:
+* The source code pointed by the NFT
+* View analytics for your app (soon)
+
+Minting fees goes to the Muse DAO and are governed by Muse holders.
