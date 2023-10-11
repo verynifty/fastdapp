1ffa1bb621b565b3653e000c1a2fc07f9888c83b
grandsmarquis <jdourlens@gmail.com>
2023-10-11 22:18:19 +0900

    tmp

--- webapp/pages/visual.js
+++ webapp/pages/visual.js
@@ -1,20 +1,30 @@
 import { Puck } from "@measured/puck";
 import "@measured/puck/dist/index.css";
 
+import { ButtonGroup, ButtonGroupProps } from "components/visualblocks/ButtonGroup";
+import { Card, CardProps } from "components/visualblocks/Card";
+import { Columns, ColumnsProps } from "components/visualblocks/Columns";
+import { Hero, HeroProps } from "components/visualblocks/Hero";
+import { Heading, HeadingProps } from "components/visualblocks/Heading";
+import { Flex, FlexProps } from "components/visualblocks/Flex";
+import { Logos, LogosProps } from "components/visualblocks/Logos";
+import { Stats, StatsProps } from "components/visualblocks/Stats";
+import { Text, TextProps } from "components/visualblocks/Text";
+import { VerticalSpace, VerticalSpaceProps } from "components/visualblocks/VerticalSpace";
 
 // Create puck component config
 const config = {
     components: {
-        HeadingBlock: {
-            fields: {
-                children: {
-                    type: "text",
-                },
-            },
-            render: ({ children }) => {
-                return <h1>{children}</h1>;
-            },
-        },
+        ButtonGroup,
+        Card,
+        Columns,
+        Hero,
+        Heading,
+        Flex,
+        Logos,
+        Stats,
+        Text,
+        VerticalSpace,
     },
 };
 
