2beb82db20c2b4f45328614ffb20fec4fc273e25
grandsmarquis <jdourlens@gmail.com>
2023-11-20 21:02:23 +0900

    Call to action and better error message

--- webapp/components/renderErrorWrapper.js
+++ webapp/components/renderErrorWrapper.js
@@ -28,13 +28,26 @@ class RenderErrorWrapper extends React.Component {
 
   render() {
     // Check if the error is thrown
+    console.log(this.state.error)
     if (this.state.hasError) {
       // You can render any custom fallback UI
       return (
-        <div>
-          <h2>Oops, there is an error!</h2>
-          {this.state.error != null ? this.state.error.toString() : ""}
-        </div>
+        <section class="w-full h-screen flex flex-col items-center justify-center bg-gray-100 dark:bg-gray-800">
+          <h1 class="text-3xl font-bold text-primary mb-4">Oops! An error occurred.</h1>
+          <div class="w-full max-w-xs md:max-w-md bg-neutral p-4 mb-4 overflow-auto">
+            <pre class="text-sm font-mono rounded  p-4 ">
+              <code>{this.state.error != null ? this.state.error.message : ""} </code>
+            </pre>
+          </div>
+          <div class="mt-6">
+            Need help?
+          </div>
+          <a
+            class="btn btn-primary mt-4"
+            href="https://t.me/grands_marquis"
+            target='_blank'
+          >Chat with a dev on Telegram</a>
+        </section>
       )
     }
 
