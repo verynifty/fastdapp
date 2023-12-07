582108d07b2b2f96d3d896fa8bf146f6b0d09137
grandsmarquis <jdourlens@gmail.com>
2023-12-07 10:39:21 +0900

    match theme

--- webapp/components/Header.jsx
+++ webapp/components/Header.jsx
@@ -81,7 +81,7 @@ const Header = (props) => {
   }
 
   return (
-    <div className="navbar bg-base-100 h-16" >
+    <div className="navbar h-16 bg-base" >
       <div className="navbar-start">
         <div className="dropdown">
           <label tabIndex={0} className="btn btn-ghost lg:hidden">
