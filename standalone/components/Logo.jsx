156b547e13e0e17fd58bd2de6b8afc1113709824
grandsmarquis <jdourlens@gmail.com>
2023-12-17 16:24:13 +0100

    clean:     rm -rf standalone/components/AuthLayout.jsx     rm -rf standalone/components/Button.jsx     rm -rf standalone/components/CallToAction.jsx     rm -rf standalone/components/Container.jsx     rm -rf standalone/components/Dapps.jsx     rm -rf standalone/components/Faqs.jsx     rm -rf standalone/components/Fields.jsx     rm -rf standalone/components/Footer.jsx     rm -rf standalone/components/Header.jsx     rm -rf standalone/components/Hero.jsx     rm -rf standalone/components/Logo.jsx     rm -rf standalone/components/NavLink.jsx     rm -rf standalone/components/Pricing.jsx     rm -rf standalone/components/PrimaryFeatures.jsx     rm -rf standalone/components/SecondaryFeatures.jsx     rm -rf standalone/components/balance.js     rm -rf standalone/components/bn.js     rm -rf standalone/components/pleaseConnect.js     rm -rf standalone/components/publish.js     rm -rf standalone/components/render.js     rm -rf standalone/components/renderErrorWrapper.js     rm -rf standalone/components/sendTransactionTwo.js

--- standalone/components/Logo.jsx
+++
@@ -1,9 +0,0 @@
-
-
-export function Logo(props) {
-  return (
-    <span className="text-2xl font-bold text-slate-900">Fast Dapp <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded text-cyan-600 bg-cyan-200 uppercase last:mr-0 mr-1">
-      alpha
-    </span></span>
-  )
-}
