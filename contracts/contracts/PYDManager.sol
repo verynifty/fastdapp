800f7947df334e1f6b9da710478888fd09a37cda
grandsmarquis <jdourlens@gmail.com>
2024-03-10 17:00:33 +0900

    pyd manager and proxy

---
+++ contracts/contracts/PYDManager.sol
@@ -0,0 +1,134 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import "./PYDProxy.sol";
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+interface IAAVE {
+    struct ReserveConfigurationMap {
+        //bit 0-15: LTV
+        //bit 16-31: Liq. threshold
+        //bit 32-47: Liq. bonus
+        //bit 48-55: Decimals
+        //bit 56: reserve is active
+        //bit 57: reserve is frozen
+        //bit 58: borrowing is enabled
+        //bit 59: stable rate borrowing enabled
+        //bit 60: asset is paused
+        //bit 61: borrowing in isolation mode is enabled
+        //bit 62: siloed borrowing enabled
+        //bit 63: flashloaning enabled
+        //bit 64-79: reserve factor
+        //bit 80-115 borrow cap in whole tokens, borrowCap == 0 => no cap
+        //bit 116-151 supply cap in whole tokens, supplyCap == 0 => no cap
+        //bit 152-167 liquidation protocol fee
+        //bit 168-175 eMode category
+        //bit 176-211 unbacked mint cap in whole tokens, unbackedMintCap == 0 => minting disabled
+        //bit 212-251 debt ceiling for isolation mode with (ReserveConfiguration::DEBT_CEILING_DECIMALS) decimals
+        //bit 252-255 unused
+        uint256 data;
+    }
+
+    struct ReserveData {
+        //stores the reserve configuration
+        ReserveConfigurationMap configuration;
+        //the liquidity index. Expressed in ray
+        uint128 liquidityIndex;
+        //the current supply rate. Expressed in ray
+        uint128 currentLiquidityRate;
+        //variable borrow index. Expressed in ray
+        uint128 variableBorrowIndex;
+        //the current variable borrow rate. Expressed in ray
+        uint128 currentVariableBorrowRate;
+        //the current stable borrow rate. Expressed in ray
+        uint128 currentStableBorrowRate;
+        //timestamp of last update
+        uint40 lastUpdateTimestamp;
+        //the id of the reserve. Represents the position in the list of the active reserves
+        uint16 id;
+        //aToken address
+        address aTokenAddress;
+        //stableDebtToken address
+        address stableDebtTokenAddress;
+        //variableDebtToken address
+        address variableDebtTokenAddress;
+        //address of the interest rate strategy
+        address interestRateStrategyAddress;
+        //the current treasury balance, scaled
+        uint128 accruedToTreasury;
+        //the outstanding unbacked aTokens minted through the bridging feature
+        uint128 unbacked;
+        //the outstanding debt borrowed against this asset in isolation mode
+        uint128 isolationModeTotalDebt;
+    }
+
+    /**
+     * @notice Supplies an `amount` of underlying asset into the reserve, receiving in return overlying aTokens.
+     * - E.g. User supplies 100 USDC and gets in return 100 aUSDC
+     * @param asset The address of the underlying asset to supply
+     * @param amount The amount to be supplied
+     * @param onBehalfOf The address that will receive the aTokens, same as msg.sender if the user
+     *   wants to receive them on his own wallet, or a different address if the beneficiary of aTokens
+     *   is a different wallet
+     * @param referralCode Code used to register the integrator originating the operation, for potential rewards.
+     *   0 if the action is executed directly by the user, without any middle-man
+     */
+    function supply(
+        address asset,
+        uint256 amount,
+        address onBehalfOf,
+        uint16 referralCode
+    ) external;
+
+    /**
+     * @notice Returns the state and configuration of the reserve
+     * @param asset The address of the underlying asset of the reserve
+     * @return The state and configuration data of the reserve
+     */
+    function getReserveData(
+        address asset
+    ) external view returns (ReserveData memory);
+}
+
+import "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import "openzeppelin-contracts/contracts/proxy/Clones.sol";
+
+contract PYDManager {
+    mapping(address => PYDProxy) public vaults;
+    ILIDO public lido = ILIDO(0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84);
+    IAAVE public aave = IAAVE(0x87870Bca3F3fD6335C3F4ce8392D69350B4fA4E2);
+    PYDProxy public proxyImplementation;
+
+    constructor() {
+        // we deploy the proxy implementation contract once
+        proxyImplementation = new PYDProxy();
+    }
+
+    function create(address _owner) public {
+        require(vaults[_owner] == PYDProxy(address(0)), "Already created");
+        vaults[_owner] = PYDProxy(Clones.clone(address(proxyImplementation)));
+        vaults[_owner].init(_owner);
+    }
+
+    // This is just a helper that enable user to deposit ETH directly and submitting it to Lido for stETH
+    function depositETHWithLido(PYDProxy _vault) public payable {
+        require(msg.value > 0, "Deposit must be greater than 0");
+        lido.submit{value: msg.value}(address(this));
+        lido.approve(address(_vault), lido.balanceOf(address(this)));
+        _vault.delegateDeposit(msg.sender, lido, lido.balanceOf(address(this)));
+    }
+
+    // This is just a helper that enable user to deposit ERC20 that are available on Aave to supply
+    function depositERC20WithAAVE(PYDProxy _vault, IERC20 _asset, uint256 _amount) public {
+        require(_amount > 0, "Deposit must be greater than 0");
+        IERC20 aToken = IERC20(aave.getReserveData(address(_asset)).aTokenAddress);
+        _asset.transferFrom(msg.sender, address(this), _amount);
+        _asset.approve(address(aave), _amount);
+        aave.supply(address(_asset), _amount, address(this), 0);
+        aToken.approve(address(_vault), aToken.balanceOf(address(this)));
+        _vault.delegateDeposit(msg.sender, aToken, aToken.balanceOf(address(this)));
+    }
+
+}
---
+++ contracts/contracts/PYDProxy.sol
@@ -0,0 +1,90 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract PYDProxy {
+    
+    address public owner;
+    address public manager;
+
+    mapping(address => uint256) public deposits;
+    uint256 public totalDeposits;
+    IERC20 public asset;
+
+    event Deposit(address indexed _user, uint256 _amount);
+    event WithdrawDeposit(address indexed _user, uint256 _amount);
+    event WithdrawRewards(address indexed _user, uint256 _amount);
+    
+    constructor() {}
+
+    function init(address _owner, address _asset) public {
+        require(owner == address(0), "PYD: already initialized");
+        owner = _owner;
+        asset = IERC20(_asset);
+        manager = msg.sender;
+    }
+
+    function transferOwnership(address _owner) public {
+        require(msg.sender == owner, "Mus be owner");
+        owner = _owner;
+    }
+
+    // deposit a token for donating yield in the vault
+    // token must be a rebasing ERC20 token such as Lido's stETH or AAVE's aUSDC..
+    function _deposit( uint256 _amount, address _from, address _to) internal {
+        asset.transferFrom(_from, address(this), _amount);
+        deposits[_to] += _amount;
+        totalDeposits += _amount;
+        emit Deposit(_to, _amount);
+    }
+
+    function deposit(uint256 _amount) public {
+        _deposit(_amount, msg.sender, msg.sender);
+    }
+
+    // Deposit tokens on behalf of someone else (e.g. from a contract)
+    function delegateDeposit(address _for, uint256 _amount) public {
+        _deposit(_amount, msg.sender, _for);
+    }
+
+    function _withdraw(address _for, uint256 _amount) internal {
+        require(deposits[_for] >= _amount, "Not enough funds");
+        deposits[_for] -= _amount;
+        totalDeposits -= _amount;
+        asset.transfer(_for, _amount);
+        emit WithdrawDeposit(_for, _amount);
+    }
+    
+    // A user can withdraw the previously deposited tokens at anytime
+    function withdraw(uint256 _amount) public {
+        _withdraw(msg.sender, _amount);
+    }
+
+    // A user can withdraw the previously deposited tokens at anytime
+    function withDrawAll() public {
+        uint256 _amount = deposits[msg.sender];
+        _withdraw(msg.sender, _amount);
+    }
+
+    // Get how much token were accrued by the pool owner
+    function getRewards() public view returns (uint256){
+        return asset.balanceOf(address(this)) - totalDeposits;
+    }
+
+    function getInfo(address user) public view returns (IERC20 _asset, uint256 _totalDeposits, uint256 _myDeposit, uint256 _rewards){
+        return (asset, totalDeposits, deposits[user], getRewards());
+    }
+
+    function withdrawRewards() public {
+        require(msg.sender == owner, "Only owner can withdraw rewards");
+        uint256 _amount = getRewards();
+        // We take a 0.3% fee on the rewards
+        uint256 fee = _amount * 100 / 10000;
+        // Transfer fee to Muse DAO multisig
+        asset.transfer(0x6fBa46974b2b1bEfefA034e236A32e1f10C5A148, fee);
+        asset.transfer(msg.sender, _amount - fee);
+        emit WithdrawRewards(msg.sender, _amount);
+    }
+
+}
